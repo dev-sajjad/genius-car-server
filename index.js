2588ba57d57802a4c52d75ea522a66d501c47cd9
dev-sajjad <sajjadbmarpc@gmail.com>
2022-12-22 23:35:02 +0600

    post orders data and get specific users orders by query

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -28,7 +28,9 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const serviceCollection = client.db('geniusCar').collection('services')
+    const orderCollection = client.db('geniusCar').collection('orders')
 
+    //service api
     //get all services data from database
     app.get("/services", async (req, res) => {
       const query = {};
@@ -36,6 +38,39 @@ async function run() {
       const service = await cursor.toArray();
       res.send(service);
     });
+
+    //get a specific service by using dynamic id from db
+    app.get('/services/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) }
+      const service = await serviceCollection.findOne(query)
+      res.send(service)
+    })
+
+
+    // order api
+
+    // get specific user order data by email query
+    app.get('/orders', async (req, res) => {
+      let query = {};
+      if (req.query.email) {
+        query = {
+          email: req.query.email
+        }
+      }
+      const cursor = orderCollection.find(query)
+      const orders = await cursor.toArray();
+      res.send(orders)
+    })
+
+    // post order data to database 
+    app.post('/orders', async (req, res) => {
+      const order = req.body
+      const result = await orderCollection.insertOne(order)
+      console.log(result)
+      res.send(result);
+    })
+
   }
   finally {
 
